02b75a3c93720fd25987e11757926fcaf0946175
Rev. Johnny Healey <rev.null@gmail.com>
2020-06-13 22:28:20 -0400

    Making some progress on befunge.
    Can now parse a string into a program.
    Added a basic unit test.

--- src/lib/noisefunge/befunge.rs
+++ src/lib/noisefunge/befunge.rs
@@ -1,14 +1,111 @@
 
+// TODO: Add lifetimes to prog and use Cow data.
+// use std::borrow::Cow;
 
-pub struct Process<'a> {
-    pid: u64,
-    input: &'a str,
-    output: &'a str
+use std::cmp::max;
+
+enum Dir { U, D, L, R }
+
+pub struct PC { pub row : u8, pub col :u8 }
+
+impl PC {
+    pub fn new(r: u8, c: u8) -> PC {
+        PC { row : r, col : c }
+    }
 }
 
-impl<'a> Process<'a> {
-    pub fn new(pid : u64, input: &'a str, output: &'a str) ->
-               Process<'a> {
-        Process { pid : pid, input : input, output : output }
+pub struct Prog { width : u8, data : Vec<u8> }
+
+impl Prog {
+
+    pub fn parse(prog: &str) -> Result<Prog, &'static str> {
+        let mut lines = Vec::new();
+        let mut longest = 0;
+        for line in prog.split("\n") {
+            lines.push(line);
+            longest = max(longest, line.bytes().count());
+        }
+        if lines.len() == 0 {
+            return Err("Empty program.");
+        }
+        if lines.len() > 255 {
+            return Err("Too many lines in program.");
+        }
+        if longest > 255 {
+            return Err("Longest line is too long.");
+        }
+        let mut mem = Vec::new();
+        for line in lines {
+            let mut count = 0;
+            for ch in line.bytes() {
+                mem.push(ch);
+                count += 1;
+            }
+            while count < longest {
+                mem.push(32); // Pad with space
+                count += 1
+            }
+        }
+        Ok(Prog { width : longest as u8, data : mem })
     }
+
+    pub fn rows(&self) -> u8 {
+        (self.data.len() / self.width as usize) as u8
+    }
+
+    pub fn cols(&self) -> u8 {
+        self.width
+    }
+
+    pub fn lookup(&self, pc : &PC) -> u8 {
+        let i = pc.row as usize * self.width as usize + pc.col as usize;
+        self.data[i]
+    }
+}
+
+pub struct ProcessState {
+    memory: Prog,
+    pc: PC,
+    dir: Dir
+}
+
+pub struct Process {
+    pid: u64,
+    input: String,
+    output: String,
+    state: Vec<ProcessState>
+}
+
+
+impl Process {
+    pub fn new(pid: u64, input: &str, output: &str, prog: Prog) ->
+               Process {
+        let st = ProcessState { memory: prog,
+                                pc: PC { col : 0, row : 0 },
+                                dir: Dir::R };
+        let mut stvec = Vec::new();
+        stvec.push(st);
+        Process { pid : pid,
+                  input : String::from(input),
+                  output : String::from(output),
+                  state : stvec }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_prog() {
+        let pr = Prog::parse("12345\n\
+                              67890\n\
+                              a").unwrap();
+        assert_eq!(pr.cols(), 5);
+        assert_eq!(pr.rows(), 3);
+        assert_eq!(pr.data, vec![49,50,51,52,53,54,55,56,57,48,97,32,32,32,32]);
+        assert_eq!(pr.lookup(&PC::new(0,0)), 49);
+        assert_eq!(pr.lookup(&PC::new(1,1)), 55);
+    }
+
 }
--- src/lib/noisefunge/mod.rs
+++ src/lib/noisefunge/mod.rs
@@ -1,26 +1,41 @@
 
 use std::collections::BTreeMap;
-use crate::noisefunge::befunge::Process;
-
-struct Engine<'a> {
-    next_pid: u64,
-    pids: BTreeMap<u64,Process<'a>>,
-}
+use std::collections::BTreeSet;
+use std::rc::Rc;
+use crate::noisefunge::befunge::{Process, Prog};
 
 mod befunge;
 
-impl<'a> Engine<'a> {
-    pub fn new() -> Engine<'a> {
-        Engine { next_pid : 1, pids : BTreeMap::new() }
+enum MessageQueue {
+    Empty { },
+    ReadBlocked { queue : Vec<u64> },
+    WriteBlocked { queue : Vec<(u8, u64)> }
+}
+
+struct Engine {
+    next_pid: u64,
+    pids: BTreeMap<u64,Process>,
+    buffers: BTreeMap<String, MessageQueue>,
+    active: BTreeSet<u64>
+}
+
+impl Engine {
+    pub fn new() -> Engine {
+        Engine { next_pid : 1,
+                 pids : BTreeMap::new() ,
+                 buffers : BTreeMap::new(),
+                 active : BTreeSet::new() }
     }
 
-    fn make_process(&mut self, input: &'a str, output: &'a str) ->
+    fn make_process(&mut self, input: &str, output: &str,
+                    prog: Prog) ->
                     &Process {
         let pid = self.next_pid;
-        let proc = Process::new(pid, input, output);
+        let proc = Process::new(pid, input, output, prog);
+
         self.next_pid += 1;
         self.pids.insert(pid, proc);
-
+        self.active.insert(pid);
         self.pids.get(&pid).unwrap()
     }
 }
