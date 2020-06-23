e35ee3e760d0351760fa9c212450aba91eb2b889
Rev. Johnny Healey <rev.null@gmail.com>
2020-06-23 19:28:01 -0400

    Port connections are working.

--- src/lib/jack.rs
+++ src/lib/jack.rs
@@ -1,6 +1,7 @@
 
 use jack::*;
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 #[derive(Clone)]
 pub struct PortConfig {
@@ -36,23 +37,42 @@ pub struct JackHandle {
     client:AsyncClient<(), MidiHandler>,
 }
 
-impl JackHandle {
+impl<'a> JackHandle {
     pub fn new(conf : &PortConfig) -> JackHandle {
         let (client, status) =
             jack::Client::new("noisefunge",
                               ClientOptions::NO_START_SERVER)
                              .expect("Failed to start jack client.");
 
-        let beats_in = client.register_port("beats_in", MidiIn::default());
-        let mut locals = Vec::new();
+        let beats_in = client.register_port("beats_in", MidiIn::default())
+                             .unwrap();
+        let mut locals = HashMap::new();
 
         for name in &conf.locals {
-            locals.push(client.register_port(name, MidiOut::default()));
+            locals.insert(name.clone(),
+                          client.register_port(name, MidiOut::default())
+                          .expect("Failed to register port"));
         }
 
         let handler = MidiHandler { };
         let active = client.activate_async((), handler)
                            .expect("Failed to activate client.");
+        let client = active.as_client();
+        for (src, dst) in &conf.connections {
+            println!("{} -> {}", src, dst);
+            let src_name = &locals.get(src).unwrap().name().unwrap();
+            for name in client.ports(Some(dst), None, PortFlags::IS_INPUT) {
+                println!("{} -> {}: {:?}", src_name, name,
+                         client.connect_ports_by_name(src_name, &name));
+            }
+        }
+
+        for name in client.ports(Some(&conf.beat_source), None,
+                                 PortFlags::IS_OUTPUT) {
+            let bi_name = &beats_in.name().unwrap();
+            println!("{} -> {}: {:?}", &name, bi_name,
+                     client.connect_ports_by_name(&name, bi_name));
+        }
         JackHandle { client : active }
     }
 }
