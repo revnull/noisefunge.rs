6718aaa90ddf55a33f5f6a6c2969c71c4d0abbbf
Rev. Johnny Healey <rev.null@gmail.com>
2020-06-22 21:11:18 -0400

    Making some progress on jack interface.

--- src/funged/main.rs
+++ src/funged/main.rs
@@ -1,6 +1,19 @@
 
-use noisefunge::*;
+use noisefunge::jack::*;
+use std::{thread, time};
 
 fn main() {
-    println!("Hello, world!");
+
+    let mut conf = PortConfig::new("jack_midi_clock");
+    conf.connect("ports1", "Qsynth1");
+    conf.connect("ports2", "Qsynth2");
+    conf.connect("ports3", "Qsynth3");
+    conf.connect("ports4", "Qsynth4");
+    conf.connect("ports5", "Qsynth5");
+
+    let mut handle = JackHandle::new(&conf);
+
+    while true  {
+        thread::sleep(time::Duration::from_secs(1));
+    }
 }
--- src/lib/jack.rs
+++ src/lib/jack.rs
@@ -1,20 +1,58 @@
 
-use jack::{Client, ClientOptions, Error, MidiOut};
+use jack::*;
+use std::collections::HashSet;
 
-struct JackHandle {
-    pub client: Client,
-    pub ports: Vec<MidiOut>
+#[derive(Clone)]
+pub struct PortConfig {
+    beat_source: String,
+    locals: HashSet<String>,
+    connections: Vec<(String, String)>
+}
+
+impl PortConfig {
+    pub fn new(beat_source: &str) -> Self {
+        PortConfig { beat_source : String::from(beat_source),
+                     locals : HashSet::new(),
+                     connections : Vec::new() }
+    }
+
+    pub fn connect(&mut self, local: &str, remote: &str) {
+        self.locals.insert(String::from(local));
+        self.connections.push((String::from(local), String::from(remote)));
+    }
+}
+
+struct MidiHandler {
+
+}
+
+impl ProcessHandler for MidiHandler {
+    fn process(&mut self, client: &Client, scope: &ProcessScope) -> Control {
+        Control::Continue
+    }
+}
+
+pub struct JackHandle {
+    client:AsyncClient<(), MidiHandler>,
 }
 
 impl JackHandle {
-    fn new() -> Result<JackHandle, Error> {
-        let (cl, status) = jack::Client::new(
-            "noisefunge",
-            ClientOptions::NO_START_SERVER)?;
-        println!("Opened client {}, with status {:?}.",
-                 cl.name(),
-                 status);
-        Ok(JackHandle { client: cl,
-                        ports: Vec::new() })
+    pub fn new(conf : &PortConfig) -> JackHandle {
+        let (client, status) =
+            jack::Client::new("noisefunge",
+                              ClientOptions::NO_START_SERVER)
+                             .expect("Failed to start jack client.");
+
+        let beats_in = client.register_port("beats_in", MidiIn::default());
+        let mut locals = Vec::new();
+
+        for name in &conf.locals {
+            locals.push(client.register_port(name, MidiOut::default()));
+        }
+
+        let handler = MidiHandler { };
+        let active = client.activate_async((), handler)
+                           .expect("Failed to activate client.");
+        JackHandle { client : active }
     }
 }
