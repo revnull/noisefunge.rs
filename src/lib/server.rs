2d3eb31a996d87bbf9112b112e836dcdcd019588
Rev. Johnny Healey <rev.null@gmail.com>
2020-06-28 17:06:57 -0400

    Handling a get request.

--- src/lib/server.rs
+++ src/lib/server.rs
@@ -1,6 +1,6 @@
 extern crate simple_server;
 
-use simple_server::{Server, Request, ResponseBuilder, ResponseResult};
+use simple_server::*;
 use std::thread;
 use std::sync::{Arc, Mutex, Condvar};
 use crossbeam_channel::{bounded, Sender, Receiver};
@@ -13,7 +13,7 @@ unsafe impl<T: Send> Send for Responder<T> {}
 
 #[derive(Debug)]
 pub enum FungeRequest {
-    StartProcess(String, String, usize, Vec<u8>, Responder<u64>),
+    StartProcess(String, String, String, Responder<Result<u64,String>>),
 }
 
 unsafe impl Send for FungeRequest {}
@@ -23,10 +23,25 @@ pub struct ServerHandle {
     pub channel: Receiver<FungeRequest>
 }
 
-fn handle_request(sender: Arc<Sender<FungeRequest>>, req: Request<Vec<u8>>,
-                  resp: ResponseBuilder) -> ResponseResult {
+fn handle_GET(sender: Arc<Sender<FungeRequest>>,
+              req: Request<Vec<u8>>, mut resp: ResponseBuilder)
+              -> ResponseResult {
+    if req.uri() == "/" {
+        return resp.status(200)
+                   .body(Vec::from("Hello, world"))
+                   .map_err(|e| Error::from(e))
+    }
 
-    panic!("foo")
+    resp.status(404).body(Vec::new()).map_err(|e| Error::from(e))
+}
+
+fn handle_request(sender: Arc<Sender<FungeRequest>>, req: Request<Vec<u8>>,
+                  mut resp: ResponseBuilder) -> ResponseResult {
+
+    match *req.method() {
+        Method::GET => handle_GET(sender, req, resp),
+        _ => resp.status(501).body(Vec::new()).map_err(|e| Error::from(e))
+    }
 }
 
 impl ServerHandle {
