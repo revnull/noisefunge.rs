a82dff57f135e77cc73f9350ccd1d4ab4a8563d4
Rev. Johnny Healey <rev.null@gmail.com>
2020-06-28 21:25:52 -0400

    Working on Responder semantics.

--- src/lib/server.rs
+++ src/lib/server.rs
@@ -4,16 +4,42 @@ use simple_server::*;
 use std::thread;
 use std::sync::{Arc, Mutex, Condvar};
 use crossbeam_channel::{bounded, Sender, Receiver};
+use http::uri::Parts;
 
 use crate::config::{FungedConfig};
 
-#[derive(Debug)]
-pub struct Responder<T>(Mutex<Option<T>>, Condvar);
+#[derive(Debug,Clone)]
+pub struct Responder<T>(Arc<(Mutex<Option<T>>, Condvar)>);
 unsafe impl<T: Send> Send for Responder<T> {}
+
+impl<T> Responder<T> {
+    fn wait_for(&self) -> T {
+        let Responder(arc) = self;
+        let lock = &arc.0;
+        let cond = &arc.1;
+
+        let mut val = lock.lock().unwrap();
+        while val.is_none() {
+            val = cond.wait(val).unwrap();
+        }
+
+        val.take().unwrap()
+    }
+
+    fn respond(&self,t: T) {
+        let Responder(arc) = self;
+        let lock = &arc.0;
+        let cond = &arc.1;
+        let mut val = lock.lock().unwrap();
+        *val = Some(t);
+        cond.notify_one();
+    }
+}
 
 #[derive(Debug)]
 pub enum FungeRequest {
     StartProcess(String, String, String, Responder<Result<u64,String>>),
+    GetProcess(u64, Responder<Option<String>>),
 }
 
 unsafe impl Send for FungeRequest {}
@@ -26,11 +52,27 @@ pub struct ServerHandle {
 fn handle_GET(sender: Arc<Sender<FungeRequest>>,
               req: Request<Vec<u8>>, mut resp: ResponseBuilder)
               -> ResponseResult {
-    if req.uri() == "/" {
+
+    let uri = req.uri();
+
+    if uri.path() == "/" {
         return resp.status(200)
                    .body(Vec::from("Hello, world"))
                    .map_err(|e| Error::from(e))
     }
+
+    println!("{}", uri.path());
+    println!("{:?}", uri.query());
+
+    resp.status(404).body(Vec::new()).map_err(|e| Error::from(e))
+}
+
+fn handle_POST(sender: Arc<Sender<FungeRequest>>,
+               req: Request<Vec<u8>>, mut resp: ResponseBuilder)
+               -> ResponseResult {
+
+    let uri = req.uri();
+
 
     resp.status(404).body(Vec::new()).map_err(|e| Error::from(e))
 }
@@ -40,6 +82,7 @@ fn handle_request(sender: Arc<Sender<FungeRequest>>, req: Request<Vec<u8>>,
 
     match *req.method() {
         Method::GET => handle_GET(sender, req, resp),
+        Method::POST => handle_POST(sender, req, resp),
         _ => resp.status(501).body(Vec::new()).map_err(|e| Error::from(e))
     }
 }
