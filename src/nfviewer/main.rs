a90864894e672238f0a0661d2c5f73403cbda135
Rev. Johnny Healey <rev.null@gmail.com>
2020-07-01 22:47:10 -0400

    Resizing window works in nfviewer.

--- src/nfviewer/main.rs
+++ src/nfviewer/main.rs
@@ -2,6 +2,9 @@
 use noisefunge::api::*;
 use noisefunge::config::*;
 use clap::{Arg, App};
+use pancurses::{initscr, cbreak, noecho, endwin, Input};
+use std::time::Duration;
+use std::thread::sleep;
 
 fn read_args() -> String {
     let matches = App::new("nfviewer")
@@ -25,4 +28,36 @@ fn main() {
 
     println!("baseuri: {}", baseuri);
 
+    let window = initscr();
+    cbreak();
+    noecho();
+    window.nodelay(true);
+    let (mut miny, mut minx) = window.get_beg_yx();
+    let (mut maxy, mut maxx) = window.get_max_yx();
+    let mut done = false;
+    let mut retile = true;
+
+    'outer: while !done {
+        window.clear();
+        window.mvaddstr(1,1,format!("{} - {}", miny, minx));
+        window.mvaddstr(2,2,format!("{} - {}", maxy, maxx));
+        window.refresh();
+        loop {
+            match window.getch() {
+                None => break,
+                Some(Input::KeyResize) => {
+                    miny = window.get_beg_y();
+                    minx = window.get_beg_x();
+                    maxy = window.get_max_y();
+                    maxx = window.get_max_x();
+                    retile = true;
+                    continue 'outer;
+                }
+                Some(i) => window.mvaddstr(3,3,format!("{:?}", i)),
+            };
+        }
+        if !done { sleep(Duration::from_millis(100)) };
+    }
+
+    endwin();
 }
